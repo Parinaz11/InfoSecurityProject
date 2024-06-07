7538452441bfdf63d80b86bfa64d9343f4366b9d
Parinaz11 <parinaz.akef@gmail.com>
2024-06-08 02:37:36 +0330

    Fixed port problem

--- Client.py
+++ Client.py
@@ -6,8 +6,7 @@ from Crypto.Cipher import PKCS1_OAEP
 
 HOST = 'localhost'
 PORT = 12345
-# P2P_PORT = 12346
-enteredThread = False
+P2P_PORT = 12346
 
 
 class Client:
@@ -27,9 +26,9 @@ class Client:
                 choice = input("Enter your choice: ")
 
                 if choice == "1":
-                    self.register_user_client()
+                    self.register_user()
                 elif choice == "2":
-                    self.login_user_client()
+                    self.login_user()
                 elif choice == "3":
                     print("Exiting...")
                     break
@@ -47,10 +46,7 @@ class Client:
     def receive_message(self):
         return self.socket.recv(1024).decode()
 
-    def register_user_client(self):
-
-        global enteredThread  # Declare the global variable
-
+    def register_user(self):
         self.send_message("register")
         email = input("Enter your email: ")
         self.send_message(email)
@@ -61,21 +57,23 @@ class Client:
         confirm_password = input("Confirm your password: ")
         self.send_message(confirm_password)
 
-        print("MAIN HEREEE")
-        message = None
-        while not enteredThread and message == None:
-            print("Waiting")
-            message = self.receive_message()
-            print(message)
-        enteredThread = False
-        print("MAIN HEREEE 222222222222222")
+        print(self.receive_message())
 
-    def login_user_client(self):
+    def login_user(self):
+
+        global P2P_PORT
+
         self.send_message("login")
         self.username = input("Enter your username: ")
         self.send_message(self.username)
         password = input("Enter your password: ")
         self.send_message(password)
+        # Get the p2p port number which is unique
+        P2P_PORT = int(self.receive_message())
+        p2p_thread = threading.Thread(target=start_p2p_server, daemon=True)
+        p2p_thread.start()
+        # self.send_message(str(P2P_PORT))
+
         print(self.receive_message())
 
     def private_chat(self):
@@ -105,146 +103,31 @@ class Client:
         recipient_socket.close()
 
 
-    def start_p2p_server(self, port):
-        p2p_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        p2p_socket.bind((HOST, port))
-        p2p_socket.listen(1)
-        print(f"P2P server listening on port {port}")
+def start_p2p_server():
+    p2p_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    p2p_socket.bind((HOST, P2P_PORT))
+    p2p_socket.listen(1)
+    print(f"P2P server listening on port {P2P_PORT}")
 
+    while True:
+        conn, addr = p2p_socket.accept()
+        print(f"Connected to {addr}")
+        threading.Thread(target=handle_p2p_client, args=(conn,)).start()
+
+
+def handle_p2p_client(conn):
+    with conn:
         while True:
-            conn, addr = p2p_socket.accept()
-            print(f"Connected to {addr}")
-            threading.Thread(target=self.handle_p2p_client, args=(conn,)).start()
+            data = conn.recv(1024)
+            if not data:
+                break
+            print("Received message:", data.decode())
 
-
-    def handle_p2p_client(conn):
-        with conn:
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print("Received message:", data.decode())
-
-
-    def handle_server_messages(self):
-        global enteredThread  # Declare the global variable
-        while True:
-
-            message = self.receive_message()
-            print("GOTTT", message)
-            if message.startswith("START_P2P_SERVER"):
-                _, port = message.split(":")
-                port = int(port)
-                self.p2p_server_thread = threading.Thread(target=self.start_p2p_server, args=(port,))
-                self.p2p_server_thread.start()
-
-            elif message:
-                print(message)
-                enteredThread = True
 
 def main():
     client = Client()
-    threading.Thread(target=client.handle_server_messages, daemon=True).start()
     client.run()
 
 
 if __name__ == "__main__":
     main()
-
-
-
-# import socket
-# import base64
-# from Crypto.PublicKey.RSA import import_key
-# from Crypto.Cipher import PKCS1_OAEP
-# from Crypto.PublicKey import RSA
-#
-# HOST = 'localhost'
-# PORT = 12345
-#
-# class Client:
-#     def __init__(self):
-#         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         self.socket.connect((HOST, PORT))
-#         self.username = None
-#
-#     def run(self):
-#         try:
-#             while True:
-#                 print("1. Register")
-#                 print("2. Login")
-#                 print("3. Exit")
-#                 print("4. Private Chat")
-#
-#                 choice = input("Enter your choice: ")
-#
-#                 if choice == "1":
-#                     self.register_user()
-#                 elif choice == "2":
-#                     self.login_user()
-#                 elif choice == "3":
-#                     print("Exiting...")
-#                     break
-#                 elif choice == "4":
-#                     self.private_chat()
-#                 else:
-#                     print("Invalid choice!")
-#
-#         except Exception as e:
-#             print("Client exception:", str(e))
-#
-#     def send_message(self, message):
-#         self.socket.sendall(message.encode())
-#
-#     def receive_message(self):
-#         return self.socket.recv(1024).decode()
-#
-#     def register_user(self):
-#         self.send_message("register")
-#         email = input("Enter your email: ")
-#         self.send_message(email)
-#         username = input("Enter your username: ")
-#         self.send_message(username)
-#         password = input("Enter your password: ")
-#         self.send_message(password)
-#         confirm_password = input("Confirm your password: ")
-#         self.send_message(confirm_password)
-#
-#         print(self.receive_message())
-#
-#     def login_user(self):
-#         self.send_message("login")
-#         self.username = input("Enter your username: ")
-#         self.send_message(self.username)
-#         password = input("Enter your password: ")
-#         self.send_message(password)
-#
-#         print(self.receive_message())
-#
-#     def private_chat(self):
-#         self.send_message("privateChat")
-#         recipient_username = input("Enter recipient username: ")
-#         self.send_message(recipient_username)
-#
-#         public_key_response = self.receive_message()
-#         if public_key_response == "PUBLIC_KEY":
-#             recipient_public_key_str = self.receive_message()
-#             recipient_public_key = RSA.importKey(recipient_public_key_str)
-#             recipient_cipher = PKCS1_OAEP.new(recipient_public_key)
-#
-#             print("Start typing your messages (type 'exit' to end chat):")
-#             while True:
-#                 message = input()
-#                 if message == "exit":
-#                     break
-#                 encrypted_message = recipient_cipher.encrypt(message.encode())
-#                 self.send_message(base64.b64encode(encrypted_message).decode())
-#         else:
-#             print("Failed to initiate private chat:", public_key_response)
-#
-# def main():
-#     client = Client()
-#     client.run()
-#
-# if __name__ == "__main__":
-#     main()
