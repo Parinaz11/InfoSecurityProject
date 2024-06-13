90c40d7e64cf4f0509ae3e587de5294916cfadf4
Parinaz11 <parinaz.akef@gmail.com>
2024-06-13 17:07:47 +0330

    bug: trying to use an improperly decoded key

--- Client.py
+++ Client.py
@@ -8,7 +8,6 @@ import base64
 
 HOST = 'localhost'
 PORT = 12345
-# P2P_PORT = 12346 # not used
 server_connection = True
 
 
@@ -17,22 +16,17 @@ class Client:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((HOST, PORT))
         self.username = None
-        # Keys
-        self.key = RSA.generate(2048)
-        self.public_key = self.key.publickey().export_key() # .decode('utf-8')
+        self.key = None
+        self.public_key = None
         self.peer_public_key = None
 
     def run(self):
-        # global server_connection
         try:
             while True:
-
-                # server_connection = True
                 print("1. Register")
                 print("2. Login")
                 print("3. Exit")
                 print("4. Private Chat")
-
                 choice = input("Enter your choice: ")
 
                 if choice == "1":
@@ -46,21 +40,21 @@ class Client:
                     self.private_chat()
                 else:
                     print("Invalid choice!")
-
         except Exception as e:
             print("Client exception:", str(e))
 
     def send_message(self, message):
-        # Sign the message with private key
         self.socket.sendall(message.encode())
 
     def receive_message(self):
-        data = self.socket.recv(1024).decode()
-        # if data.startswith("PEERPK"):
-        #     self.peer_public_key = data[6:].encode('utf-8')
-        #     dfdjfkdfjdf
-        #     print("Received peer's public key.")
-        return data
+        return self.socket.recv(1024).decode()
+
+    def receive_keys(self):
+        private_key_pem = self.receive_message()
+        public_key_pem = self.receive_message()
+        private_key = private_key_pem.encode('utf-8')  # RSA.import_key(private_key_pem)
+        public_key = public_key_pem.encode('utf-8')  # RSA.import_key(public_key_pem)
+        return private_key, public_key
 
     def register_user(self):
         self.send_message("register")
@@ -73,25 +67,29 @@ class Client:
         confirm_password = input("Confirm your password: ")
         self.send_message(confirm_password)
 
+        self.key, self.public_key = self.receive_keys()
         print(self.receive_message())
 
     def login_user(self):
-
         self.send_message("login")
         self.username = input("Enter your username: ")
         self.send_message(self.username)
         password = input("Enter your password: ")
         self.send_message(password)
-        # Get the p2p port number which is unique
-        user_port = int(self.receive_message())
+
+        user_port = self.receive_message()
+        try:
+            user_port = int(user_port)  # Convert user_port to integer
+        except ValueError:
+            print("Invalid port number received from the server.")
+            return
+
         p2p_thread = threading.Thread(target=self.start_p2p_server, args=(user_port,), daemon=True)
         p2p_thread.start()
-        # self.send_message(str(P2P_PORT))
 
         print(self.receive_message())
 
     def private_chat(self):
-
         self.send_message("privateChat")
         recipient_username = input("Enter recipient username: ")
         self.send_message(recipient_username)
@@ -99,10 +97,9 @@ class Client:
         p2p_info_confirm = self.receive_message()
         if p2p_info_confirm.startswith("P2P_INFO"):
             p2p_info = self.receive_message()
-            # Getting the public key of the other client
             received_public_key_pem = self.receive_message()[6:].strip()
             try:
-                self.peer_public_key = received_public_key_pem.encode('utf-8')
+                self.peer_public_key = RSA.import_key(received_public_key_pem)
             except Exception as e:
                 print("Failed to import peer's public key:", str(e))
                 return
@@ -120,80 +117,56 @@ class Client:
         print("Start typing your messages (type 'exit' to end chat):")
         while True:
             message = input()
+            if message == "exit":
+                break
 
-            if self.peer_public_key is not None:
-                recipient_key = RSA.import_key(self.peer_public_key)
+            if self.peer_public_key:
+                recipient_key = self.peer_public_key
                 cipher_rsa = PKCS1_OAEP.new(recipient_key)
                 encrypted_message = cipher_rsa.encrypt(message.encode('utf-8'))
 
-                # Sign the message
                 h = SHA256.new(message.encode('utf-8'))
                 signature = pkcs1_15.new(self.key).sign(h)
 
-                # Combine username, encrypted message and signature
                 final_message = f"{self.username}:{base64.b64encode(encrypted_message).decode()}:{base64.b64encode(signature).decode()}"
                 recipient_socket.sendall(final_message.encode())
-
             else:
                 print("Public key of the recipient is not available.")
-
-            if message == "exit":
-                break
-            # recipient_socket.sendall(message.encode())
 
         print("Ended conversation")
         recipient_socket.close()
 
-
-    def start_p2p_server(self, P2P_PORT):
+    def start_p2p_server(self, p2p_port):
         p2p_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        p2p_socket.bind((HOST, P2P_PORT))
+        p2p_socket.bind((HOST, p2p_port))
         p2p_socket.listen(1)
-        print(f"P2P server listening on port {P2P_PORT}")
+        print(f"P2P server listening on port {p2p_port}")
 
         while True:
             conn, addr = p2p_socket.accept()
-            print(f"1Connected to {addr}")
-            print(f"Connection:{conn}")
+            print(f"Connected to {addr}")
             threading.Thread(target=self.handle_p2p_client, args=(conn,)).start()
-
 
     def handle_p2p_client(self, conn):
         with conn:
             while True:
-
-                message = "Not received"
-                data = conn.recv(1024) # self.socket.recv(4096)
-
+                data = conn.recv(1024)
                 if not data:
                     break
 
-                if data:
-                    print("ENTERED")
-                    # if not server_connection:
-                    # Process the received message
-                    username, encrypted_message, signature = data.decode().split(":")
-                    encrypted_message = base64.b64decode(encrypted_message.encode('utf-8')) # ADDED ENCODE
-                    signature = base64.b64decode(signature)
+                username, encrypted_message, signature = data.decode().split(":")
+                encrypted_message = base64.b64decode(encrypted_message)
+                signature = base64.b64decode(signature)
 
-                    # Decrypt the message
-                    cipher_rsa = PKCS1_OAEP.new(self.key)
-                    message = cipher_rsa.decrypt(encrypted_message).decode('utf-8')
+                cipher_rsa = PKCS1_OAEP.new(self.key)
+                message = cipher_rsa.decrypt(encrypted_message).decode('utf-8')
 
-                    # Verify the signature
-                    h = SHA256.new(message.encode())
-                    try:
-                        pkcs1_15.new(RSA.import_key(self.peer_public_key)).verify(h, signature)
-                        message = f"Received verified message from {username}: {message}"
-                    except (ValueError, TypeError):
-                        message = "The signature is not valid."
-
-                print(message)
-
-                # data = conn.recv(1024)
-                # if not data:
-                #     break
-                # print("Received message:", data.decode())
+                h = SHA256.new(message.encode())
+                try:
+                    pkcs1_15.new(self.peer_public_key).verify(h, signature)
+                    print(f"Received verified message from {username}: {message}")
+                except (ValueError, TypeError):
+                    print("The signature is not valid.")
 
 
 def main():
--- Server.py
+++ Server.py
@@ -3,13 +3,14 @@ import threading
 import hashlib
 import base64
 import os
-from Cryptodome.PublicKey import RSA
+from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
 PORT = 12345
 HOST = 'localhost'
 p2p_port = 12346
 num_ports = 0
+
 
 class User:
     def __init__(self, email, username, password_hash, salt, public_key, private_key, address=None, p2p_port=None):
@@ -27,7 +28,7 @@ class UserManager:
     def __init__(self):
         self.users = []
 
-    def register_user(self, email, username, password, confirm_password):
+    def register_user(self, email, username, password, confirm_password, key_pair, public_key_pem):
         if password != confirm_password:
             return False
 
@@ -36,8 +37,7 @@ class UserManager:
 
         salt = self.generate_salt()
         hashed_password = self.hash_password(password, salt)
-        key_pair = RSA.generate(2048)
-        public_key_pem = key_pair.publickey().export_key()  # .decode('utf-8')
+
         user = User(email, username, hashed_password, salt, public_key_pem, key_pair)
         self.users.append(user)
         return True
@@ -91,10 +91,8 @@ class ClientHandler(threading.Thread):
                         self.handle_private_chat_request()
                     else:
                         self.send_message("Unknown command!")
-
             except Exception as e:
                 print("ClientHandler exception:", str(e))
-
             finally:
                 if self.username:
                     with user_handlers_lock:
@@ -112,7 +110,14 @@ class ClientHandler(threading.Thread):
         username = self.receive_message()
         password = self.receive_message()
         confirm_password = self.receive_message()
-        success = self.user_manager.register_user(email, username, password, confirm_password)
+
+        key_pair = RSA.generate(2048)
+        public_key_pem = key_pair.publickey().export_key()
+
+        self.send_message(key_pair.export_key().decode('utf-8'))
+        self.send_message(public_key_pem.decode('utf-8'))
+
+        success = self.user_manager.register_user(email, username, password, confirm_password, key_pair, public_key_pem)
         self.send_message("Registration successful!" if success else "Registration failed!")
 
     def handle_login(self):
@@ -122,11 +127,10 @@ class ClientHandler(threading.Thread):
         self.username = self.receive_message()
         password = self.receive_message()
         address = self.socket.getpeername()[0]
-        # send a unique port number
+
         num_ports += 1
         unique_port = p2p_port + num_ports
         self.send_message(str(unique_port))
-        # p2p_port = int(self.receive_message())
 
         success = self.user_manager.login_user(self.username, password, address, unique_port)
         self.send_message("Login successful!" if success else "Login failed!")
@@ -138,14 +142,10 @@ class ClientHandler(threading.Thread):
     def handle_private_chat_request(self):
         recipient_username = self.receive_message()
         recipient_user = self.user_manager.find_user_by_username(recipient_username)
-        print("Found", recipient_user)
         if recipient_user:
-            print("Sending info")
             self.send_message("P2P_INFO")
             self.send_message(f"{recipient_user.address}:{recipient_user.p2p_port}")
-            # Send the public key for that recipient
             self.send_message(f"PEERPK{recipient_user.public_key.decode('utf-8')}")
-            print("Finished sending pubic key")
         else:
             self.send_message("User does not exist.")
 
@@ -171,174 +171,3 @@ if __name__ == "__main__":
     user_handlers_lock = threading.Lock()
     main()
 
-
-
-
-# import socket
-# import threading
-# import hashlib
-# import base64
-# import os
-# from Cryptodome.PublicKey import RSA
-# from Crypto.Cipher import PKCS1_OAEP
-#
-# PORT = 12345
-# HOST = 'localhost'
-# p2p_port = 12346
-# num_ports = 0
-#
-# class User:
-#     def __init__(self, email, username, password_hash, salt, public_key, private_key, address=None, p2p_port=None):
-#         self.email = email
-#         self.username = username
-#         self.password_hash = password_hash
-#         self.salt = salt
-#         self.public_key = public_key
-#         self.private_key = private_key
-#         self.address = address
-#         self.p2p_port = p2p_port
-#
-#
-# class UserManager:
-#     def __init__(self):
-#         self.users = []
-#
-#     def register_user(self, email, username, password, confirm_password):
-#         if password != confirm_password:
-#             return False
-#
-#         if self.email_exists(email):
-#             return False
-#
-#         salt = self.generate_salt()
-#         hashed_password = self.hash_password(password, salt)
-#         key_pair = RSA.generate(2048)
-#         user = User(email, username, hashed_password, salt, key_pair.publickey(), key_pair)
-#         self.users.append(user)
-#         return True
-#
-#     def login_user(self, username, password, address, p2p_port):
-#         user = self.find_user_by_username(username)
-#         if user:
-#             hashed_input_password = self.hash_password(password, user.salt)
-#             if hashed_input_password == user.password_hash:
-#                 user.address = address
-#                 user.p2p_port = p2p_port
-#                 return True
-#         return False
-#
-#     def email_exists(self, email):
-#         for user in self.users:
-#             if user.email == email:
-#                 return True
-#         return False
-#
-#     def find_user_by_username(self, username):
-#         for user in self.users:
-#             if user.username == username:
-#                 return user
-#         return None
-#
-#     def generate_salt(self):
-#         return base64.b64encode(os.urandom(16)).decode()
-#
-#     def hash_password(self, password, salt):
-#         return hashlib.sha256(salt.encode() + password.encode()).hexdigest()
-#
-#
-# class ClientHandler(threading.Thread):
-#     def __init__(self, socket, user_manager):
-#         super().__init__()
-#         self.socket = socket
-#         self.user_manager = user_manager
-#         self.username = None
-#
-#     def run(self):
-#         with self.socket:
-#             try:
-#                 while True:
-#                     command = self.receive_message()
-#                     if command == "register":
-#                         self.handle_registration()
-#                     elif command == "login":
-#                         self.handle_login()
-#                     elif command == "privateChat":
-#                         self.handle_private_chat_request()
-#                     else:
-#                         self.send_message("Unknown command!")
-#
-#             except Exception as e:
-#                 print("ClientHandler exception:", str(e))
-#
-#             finally:
-#                 if self.username:
-#                     with user_handlers_lock:
-#                         if self.username in user_handlers:
-#                             del user_handlers[self.username]
-#
-#     def receive_message(self):
-#         return self.socket.recv(1024).decode()
-#
-#     def send_message(self, message):
-#         self.socket.sendall(message.encode())
-#
-#     def handle_registration(self):
-#         email = self.receive_message()
-#         username = self.receive_message()
-#         password = self.receive_message()
-#         confirm_password = self.receive_message()
-#         success = self.user_manager.register_user(email, username, password, confirm_password)
-#         self.send_message("Registration successful!" if success else "Registration failed!")
-#
-#     def handle_login(self):
-#         global num_ports
-#         global p2p_port
-#
-#         self.username = self.receive_message()
-#         password = self.receive_message()
-#         address = self.socket.getpeername()[0]
-#         # send a unique port number
-#         num_ports += 1
-#         unique_port = p2p_port + num_ports
-#         self.send_message(str(unique_port))
-#         # p2p_port = int(self.receive_message())
-#
-#         success = self.user_manager.login_user(self.username, password, address, unique_port)
-#         self.send_message("Login successful!" if success else "Login failed!")
-#
-#         if success:
-#             with user_handlers_lock:
-#                 user_handlers[self.username] = self
-#
-#     def handle_private_chat_request(self):
-#         recipient_username = self.receive_message()
-#         recipient_user = self.user_manager.find_user_by_username(recipient_username)
-#         if recipient_user:
-#             self.send_message("P2P_INFO")
-#             self.send_message(f"{recipient_user.address}:{recipient_user.p2p_port}")
-#             # Send the public key for that recipient
-#             self.send_message(f"{recipient_user.public_key}") # PEERPK
-#         else:
-#             self.send_message("User does not exist.")
-#
-#
-# def main():
-#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-#         server_socket.bind((HOST, PORT))
-#         server_socket.listen()
-#
-#         print(f"Server is listening on port {PORT}")
-#
-#         while True:
-#             client_socket, addr = server_socket.accept()
-#             print(f"New connection from {addr}")
-#
-#             client_handler = ClientHandler(client_socket, user_manager)
-#             client_handler.start()
-#
-#
-# if __name__ == "__main__":
-#     user_manager = UserManager()
-#     user_handlers = {}
-#     user_handlers_lock = threading.Lock()
-#     main()
